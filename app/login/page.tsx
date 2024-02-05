2511a20ef1d51d2e15bdadc7f9f2971119b2271e
Arjun Thomas <thomas.4411@osu.edu>
2024-02-05 10:55:00 -0500

    Modified form to use useState hook object

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,20 +2,20 @@
 import { initFirebase } from "@/firebase/firebase";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import Link from 'next/link';
+import { useState } from "react";
 
 export default function LoginPage() { 
-
+    const [formData, setFormData] = useState ({
+        email: "",
+        password: "" 
+    })
+    
     const app = initFirebase();
     const auth = getAuth();
-    let email = ""; 
-    let password = ""; 
 
     const logIn = async() => { 
-        email = (document.getElementById('email') as HTMLInputElement).value;
-        password = (document.getElementById('password') as HTMLInputElement).value;
-        signInWithEmailAndPassword(auth, email, password)
+        signInWithEmailAndPassword(auth, formData.email, formData.password)
             .then((userCredential) => {
-                <Link href = "/"/>
                 const user = userCredential.user;
                 console.log(user);
             })
@@ -33,13 +33,13 @@ export default function LoginPage() {
             <div className="flex justify-center">
                 <div className="pb-6">
                     <p className="pb-2"> Email or username </p>
-                    <input type="text" id="email" placeholder="Email or username" className = "border-white rounded-md border-2 w-80 pl-2 py-2 text-white bg-black"/>
+                    <input type="text"onChange={(e) => setFormData({...formData, email: e.target.value})} value = {formData.email} placeholder="Email or username" className = "border-white rounded-md border-2 w-80 pl-2 py-2 text-white bg-black"/>
                 </div>
             </div>
             <div className="flex justify-center">
                 <div className = "pb-10">
                     <p className="pb-2"> Password </p>
-                    <input type="password" id="password" placeholder = "Password" className="border-white rounded-md border-2 w-80 pl-2 py-2 text-white bg-black"/>
+                    <input type="password" onChange={(e) => setFormData({...formData, password: e.target.value})} value = {formData.password} placeholder = "Password" className="border-white rounded-md border-2 w-80 pl-2 py-2 text-white bg-black"/>
                 </div>
             </div>
             <div className = "pb-8 flex justify-center">
--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -2,36 +2,47 @@
 import { initFirebase } from "@/firebase/firebase";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function SignUpPage() { 
-
+    const [formData, setFormData] = useState ({
+        email: "",
+        password: "" 
+    })
+    
+    
     const app = initFirebase();
     const auth = getAuth();
-    let email = ""; 
-    let password = ""; 
 
     const signUp = async() => { 
-        email = (document.getElementById('email') as HTMLInputElement).value;
-        password = (document.getElementById('password') as HTMLInputElement).value;
-        const result = await createUserWithEmailAndPassword(auth,email,password);
-        console.log(result.user);
+        createUserWithEmailAndPassword(auth, formData.email, formData.password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            console.log(user.email);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
     }
 
     return (
-        <div className = "py-10 px-24 bg-black rounded-lg flex-col items-center text-white shadow-lg"> 
+        <form className = "py-10 px-24 bg-black rounded-lg flex-col items-center text-white shadow-lg"> 
             <div className= "flex justify-center">   
                 <h1 className= "text-4xl flex justify-center pb-10 font-semibold"> Sign up </h1>
             </div>
             <div className="flex justify-center">
                 <div className="pb-6">
                     <p className="pb-2"> Email or username </p>
-                    <input type="text" id="email" placeholder="Email or username" className = "border-white rounded-md border-2 w-80 pl-2 py-2 text-white bg-black"/>
+                    <input type="text" onChange={(e) => setFormData({...formData, email: e.target.value})} value={formData.email} placeholder="Email or username" className = "border-white rounded-md border-2 w-80 pl-2 py-2 text-white bg-black"/>
                 </div>
             </div>
             <div className="flex justify-center">
                 <div className = "pb-10">
                     <p className="pb-2"> Password </p>
-                    <input type="password" id="password" placeholder = "Password" className="border-white rounded-md border-2 w-80 pl-2 py-2 text-white bg-black"/>
+                    <input type="password" onChange={(e) => setFormData({...formData, password: e.target.value})} value = {formData.password} placeholder = "Password" className="border-white rounded-md border-2 w-80 pl-2 py-2 text-white bg-black"/>
                 </div>
             </div>
             <div className = "pb-8 flex justify-center">
@@ -47,7 +58,7 @@ export default function SignUpPage() {
             <div className="text-sm"> 
                 <p className="text-gray-500 text-center"> Have an account already? <Link href = "/login"><u className="text-white">Log in to Harmony</u></Link></p>
             </div>
-        </div>
+        </form>
         
     ); 
 }
