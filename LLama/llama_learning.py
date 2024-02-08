5583c85c0cc10bdc03768381ac2eefab4b27c50f
tobymcguire0 <tobymcguire461@gmail.com>
2024-02-08 15:22:57 -0500

    Rewrote llama_learning to use cmd args and be generally better

--- LLama/llama_learning.py
+++ LLama/llama_learning.py
@@ -1,29 +1,54 @@
 from llama_cpp import Llama
 from llama_cpp import LlamaGrammar
+import argparse
 
-model_path = input("Enter the path to the model:\n")
-model = Llama(model_path=model_path)
 
-grammar = LlamaGrammar.from_file("./grammars/grammar.gbnf")
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="Song Feel Generator",
+        description="Generates a song data JSON file that fits the given prompt",
+    )
+    parser.add_argument("-m", "--model", help="Path to the LLM model to use")
+    parser.add_argument(
+        "-g",
+        "--grammar",
+        help="Path to the grammar file to use",
+        default="./grammars/grammar.gbnf",
+        type=str,
+    )
+    return parser.parse_args()
 
-prompt = input("Enter a prompt:\n")
 
-max_tokens = 250
-temperature = 0.3
-top_p = 0.1
-echo = False
-stop = ["Q"]
+def generate_response(prompt, model, grammar):
+    max_tokens = 250
+    temperature = 0.3
+    top_p = 0.1
+    echo = False
+    stop = ["Q"]
 
-model_output = model(
-    prompt,
-    max_tokens=max_tokens,
-    temperature=temperature,
-    top_p=top_p,
-    echo=echo,
-    stop=stop,
-    grammar=grammar,
-)
-print("True output:\n")
-print(model_output)
-print("\nStripped Result:\n")
-print(model_output["choices"][0]["text"].strip())
+    model_output = model(
+        prompt,
+        max_tokens=max_tokens,
+        temperature=temperature,
+        top_p=top_p,
+        echo=echo,
+        stop=stop,
+        grammar=grammar,
+    )
+    return model_output["choices"][0]["text"].strip()
+
+
+def main():
+    args = parse_args()
+
+    model = Llama(model_path=args.model)
+    grammar = LlamaGrammar.from_file(args.grammar)
+    prompt = input("Enter a prompt:\n")
+
+    json = generate_response(prompt, model, grammar)
+    print("Model Output:\n")
+    print(json)
+
+
+if __name__ == "___main__":
+    main()
