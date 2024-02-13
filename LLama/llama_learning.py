07d6218652132f5e29ed78c9d3d0405bd5da242c
tobymcguire0 <tobymcguire461@gmail.com>
2024-02-13 15:35:35 -0500

    Updated Lamacpp to have better prompt engineering

--- LLama/llama_learning.py
+++ LLama/llama_learning.py
@@ -20,14 +20,53 @@ def parse_args():
 
 
 def generate_response(prompt, model, grammar):
-    max_tokens = 250
+    max_tokens = 512
     temperature = 0.3
     top_p = 0.1
     echo = False
     stop = ["Q"]
 
+    ctx_prompt = (
+        """
+The Spotify database allows searching by Acousticness, Danceability, Energy, Instrumentalness, Liveness, Loudness, Popularity, Speechiness, Time Signature, and Valence. Use these parameters to construct your queries.
+Do NOT add any text outside of the JSON format shown in the examples.
+
+Example 1: "Give me an unpopular song that was played live"
+{
+"accousticness":0.8,
+"danceability":0.2,
+"energy":0.3,
+"instrumentalness":0.7,
+"liveness":1,
+"loudness":10,
+"popularity":0.1,
+"speechiness":0.8,
+"time_signature":0.8,
+"valence":0.2
+}
+
+Example 2: "I need a track for a quiet evening."
+{
+"accousticness":0.5,
+"danceability":0.1,
+"energy":0.2,
+"instrumentalness":0.8,
+"liveness":.4,
+"loudness":5,
+"popularity":0.7,
+"speechiness":0.5,
+"time_signature":0.4,
+"valence":0.6
+}
+
+Now, your turn: "
+"""
+        + prompt
+        + '"'
+    )
+
     model_output = model(
-        prompt,
+        ctx_prompt,
         max_tokens=max_tokens,
         temperature=temperature,
         top_p=top_p,
@@ -41,7 +80,7 @@ def generate_response(prompt, model, grammar):
 def main():
     args = parse_args()
 
-    model = Llama(model_path=args.model)
+    model = Llama(model_path=args.model, n_gpu_layers=-1, n_batch=512, n_ctx=512)
     grammar = LlamaGrammar.from_file(args.grammar)
     prompt = input("Enter a prompt:\n")
 
