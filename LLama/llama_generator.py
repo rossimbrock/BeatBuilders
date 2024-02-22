53d75ba32aba069dd834748d020b745495920302
tobymcguire0 <tobymcguire461@gmail.com>
2024-02-22 15:36:55 -0500

    Added the ability for the llm to ignore some attributes

--- LLama/llama_generator.py
+++ LLama/llama_generator.py
@@ -1,5 +1,8 @@
-from llama_cpp import Llama
-from llama_cpp import LlamaGrammar
+from langchain_community.llms import LlamaCpp
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 import argparse
 
 
@@ -19,15 +22,19 @@ def parse_args():
     return parser.parse_args()
 
 
-def generate_response(prompt, model, grammar="./grammars/grammar.gbnf"):
+def generate_response(prompt, model_path, grammar_path="./grammars/grammar.gbnf"):
+
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
     max_tokens = 512
+    n_gpu_layers = 4
+    n_batch = 512
+    ctx_size = 4096
     temperature = 0.3
-    top_p = 0.1
+    top_p = 0.8
     echo = False
     stop = ["Q"]
 
-    ctx_prompt = (
-        """
+    ctx_prompt = """
 The Spotify database allows searching by Acousticness, Danceability, Energy, Instrumentalness, Liveness, Loudness, Popularity, Speechiness, Time Signature, and Valence. Use these parameters to construct your queries.
 I will define each attribute below, you will output a JSON format of all attributes that match the mood of the prompt to an equivalent numeric value.
 
@@ -36,38 +43,47 @@ I will define each attribute below, you will output a JSON format of all attribu
 "energy": How energetic the song is from 0 to 1
 "instrumentalness": Predict if the prompted track has no vocals. 0 being completely spoken and 1 being no vocals.
 "liveness": How likely the song is performed live
-"loudness": Average song loudness in decibels
+"loudness": How loud the song the prompt is describing is in decibels
 "popularity": Percentile of popularity of the prompt
 "speechiness": How much talking should be in the song described from the prompt, 1 being mostly speech.
 "time_signature": The estimated time signature of the prompt
 "valence": How happy or sad the song sounds. 0 being extremely sad and 1 being extremely happy
 
-Now, estimate the song statistics of a song following this prompt: "
-"""
-        + prompt
-        + '"'
-    )
+don't include an attribute in the responce ONLY if you are unsure about what value an attribute should be based on the prompt.
 
-    model_output = model(
-        ctx_prompt,
+Now, estimate the song statistics of a song following this prompt: {question}"
+"""
+
+    template = PromptTemplate.from_template(ctx_prompt)
+
+    llm = LlamaCpp(
+        model_path=model_path,
         max_tokens=max_tokens,
         temperature=temperature,
         top_p=top_p,
         echo=echo,
+        f16_kv=True,
         stop=stop,
-        grammar=grammar,
+        grammar_path=grammar_path,
+        verbose=True,
+        callback_manager=callback_manager,
+        n_ctx=ctx_size,
+        n_gpu_layers=n_gpu_layers,
+        n_batch=n_batch,
     )
-    return model_output["choices"][0]["text"].strip()
+
+    llm_chain = LLMChain(prompt=template, llm=llm)
+
+    model_output = llm_chain.invoke(prompt)
+    return model_output["text"].strip()
 
 
 def main():
     args = parse_args()
 
-    model = Llama(model_path=args.model, n_gpu_layers=-1, n_batch=512, n_ctx=512)
-    grammar = LlamaGrammar.from_file(args.grammar)
     prompt = input("Enter a prompt:\n")
 
-    json = generate_response(prompt, model, grammar)
+    json = generate_response(prompt, args.model, args.grammar)
     print("Model Output:\n")
     print(json)
 
