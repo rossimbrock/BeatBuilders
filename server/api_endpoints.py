be568ba6c02ccea08361cf041c43b0766ce1fa09
Arjun Thomas <thomas.4411@osu.edu>
2024-02-29 18:09:36 -0500

    Made search query global for access in other files

--- server/api_endpoints.py
+++ server/api_endpoints.py
@@ -4,7 +4,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-user_search_query = "" 
+user_search_query = ""  
+
 @app.route('/searchQuery', methods = ['POST'])
 def process_search_data(): 
     data = request.get_json()
@@ -13,7 +14,8 @@ def process_search_data():
     return search_query
 
 def save_search_query(search_query): 
-    user_search_query = save_search_query
+    global user_search_query;
+    user_search_query = search_query
 
 if __name__ == '__main__':
     app.run()
