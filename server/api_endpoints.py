ec18d229667181bf1aa65b93e25e5ee348f73f46
Arjun Thomas <thomas.4411@osu.edu>
2024-02-29 14:34:38 -0500

    Added helper function to store search query data for export to other python scripts

--- server/api_endpoints.py
+++ server/api_endpoints.py
@@ -4,12 +4,16 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+user_search_query = "" 
 @app.route('/searchQuery', methods = ['POST'])
 def process_search_data(): 
     data = request.get_json()
     search_query = data['search_data']
-    print(search_query)
+    save_search_query(search_query)
     return search_query
+
+def save_search_query(search_query): 
+    user_search_query = save_search_query
 
 if __name__ == '__main__':
     app.run()
