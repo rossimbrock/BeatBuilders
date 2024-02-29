687af8aa5e8bf1587142b5bb9f79848bfc3b14ad
Arjun Thomas <thomas.4411@osu.edu>
2024-02-29 15:42:32 -0500

    Removed firebase api key from git tracking

--- firebase/firebase.tsx
+++ firebase/firebase.tsx
@@ -2,28 +2,10 @@
 
 import { initializeApp } from "firebase/app";
 
+import { firebaseConfig } from "./config";
 // TODO: Add SDKs for Firebase products that you want to use
 
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-
-const firebaseConfig = {
-
-  apiKey: "AIzaSyCEJ1IE2QuTmBxhplhVDPpWHgHcuL92Sdw",
-
-  authDomain: "harmony-hub-83f61.firebaseapp.com",
-
-  projectId: "harmony-hub-83f61",
-
-  storageBucket: "harmony-hub-83f61.appspot.com",
-
-  messagingSenderId: "943957569696",
-
-  appId: "1:943957569696:web:ea918a0b178502df8299c5"
-
-};
 
 
 // Initialize Firebase
