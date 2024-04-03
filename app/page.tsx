67cbee38bcf5d959fbb6488bcd62373599a06859
Arjun Thomas <thomas.4411@osu.edu>
2024-04-03 16:41:53 -0400

    Attempted to implement logic for dislike button

--- app/page.tsx
+++ app/page.tsx
@@ -59,6 +59,10 @@ export default function Home({ Component, pageProps }: AppProps) {
     setSongQueue(prevQueue => prevQueue.slice(1));
   }
 
+ const changeSongOnDislike = (track: Track) =>  {
+    setSongQueue(prevQueue => prevQueue.slice(1))
+}
+
   const saveSelectionToLocal = (tracks: Track[]) => {
     const serializedSelection = JSON.stringify(tracks);
     localStorage.setItem('userSelections', serializedSelection);
@@ -97,10 +101,10 @@ export default function Home({ Component, pageProps }: AppProps) {
       </div> 
       <div className="flex justify-center pb-24"> 
         <div className="w-2/5 flex justify-end pr-4 "> 
-              <SongCard track={songCardOne} addSongToList={addSongToChosenList} />
+              <SongCard track={songCardOne} addSongToList={addSongToChosenList} switchSongs={changeSongOnDislike}/>
         </div>
         <div className = "w-2/5 flex justify-start pl-4">
-              <SongCard track={songCardTwo} addSongToList={addSongToChosenList}/>
+              <SongCard track={songCardTwo} addSongToList={addSongToChosenList} switchSongs={changeSongOnDislike}/>
           </div>
         </div>
         <GeneratedPlaylist tracks={chosenSongList} setTracks={setChosenSongList}/>
